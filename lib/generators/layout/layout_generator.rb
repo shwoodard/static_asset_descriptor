227a5e1d9ce807a2164cd7f90ef178ca691fb8a7
shwoodard <sam.h.woodard@gmail.com>
2009-11-21 10:11:35 -0800

    added an application layout generator

--- lib/generators/layout/layout_generator.rb
+++ lib/generators/layout/layout_generator.rb
@@ -1,21 +1,16 @@
 class LayoutGenerator < Rails::Generator::NamedBase
-	def manifest
-	  record do |m|
-      base_path, file_name = get_base_path_and_file_name(file_path)
-      puts actions.inspect
-	    m.directory "app/views/layouts"
-	    m.directory "public/stylesheets/#{base_path}"
-	    m.directory "public/javascripts/#{base_path}"
-	    m.directory "static/stylesheets/#{base_path}"
-	    m.directory "static/javascripts/#{base_path}"
-	    m.template 'layout.html.erb', "app/views/layouts/#{base_name}.html.erb"
-      # m.template 'stylesheet.yml.erb', "static/stylesheets/views/#{file_path}.yml" 
-      # m.template 'javascript.yml.erb', "static/javascripts/views/#{file_path}.yml"
-      # m.file 'view.css.erb', "public/stylesheets/views/#{file_path}.css"
-      # m.file 'view.js.erb', "public/javascripts/views/#{file_path}.js"
-	  end
-	end
-  def get_base_path_and_file_name(file_path)
-    ["views/#{file_path}".split('/')[0...-1].join('/'), "views/#{file_path}".split('/')[-1]]
+  def manifest
+    record do |m|
+      m.directory "app/views/layouts"
+      m.directory "public/stylesheets/common"
+      m.directory "public/javascripts/common"
+      m.directory "static/stylesheets/common"
+      m.directory "static/javascripts/common"
+      m.template 'layout.html.erb', "app/views/layouts/application.html.erb"
+      m.file 'global_stylesheet.yml.erb', "static/stylesheets/common/global.yml" 
+      m.file 'global_javascript.yml.erb', "static/javascripts/common/global.yml"
+      m.file 'application.css.erb', "public/stylesheets/common/application.css"
+      m.file 'application.js.erb', "public/javascripts/common/application.js"
+    end
   end
 end

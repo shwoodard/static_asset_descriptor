a21734a8102d33060309457a8be95f628656e32d
Sam Woodard <sam.woodard@wildfireapp.com>
2010-06-19 21:03:32 -0700

    updating for rails3

--- lib/generators/view/view_generator.rb
+++ lib/generators/view/view_generator.rb
@@ -1,19 +1,20 @@
-class ViewGenerator < Rails::Generator::NamedBase
-  def manifest
-    record do |m|
-      base_path, file_name = get_base_path_and_file_name(file_path)
-      m.directory "app/#{base_path}"
-      m.directory "public/stylesheets/#{base_path}"
-      m.directory "public/javascripts/#{base_path}"
-      m.directory "static/stylesheets/#{base_path}"
-      m.directory "static/javascripts/#{base_path}"
-      m.template 'view.html.erb', "app/views/#{file_path}.html.erb"
-      m.template 'stylesheet.yml.erb', "static/stylesheets/views/#{file_path}.yml" 
-      m.template 'javascript.yml.erb', "static/javascripts/views/#{file_path}.yml"
-      m.file 'view.css.erb', "public/stylesheets/views/#{file_path}.css"
-      m.file 'view.js.erb', "public/javascripts/views/#{file_path}.js"
-    end
+class ViewGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("../templates", __FILE__)
+  
+  def create_view
+    base_path, file_name = get_base_path_and_file_name(file_path)
+    directory "app/#{base_path}"
+    directory "public/stylesheets/#{base_path}"
+    directory "public/javascripts/#{base_path}"
+    directory "static/stylesheets/#{base_path}"
+    directory "static/javascripts/#{base_path}"
+    template 'view.html.erb', "app/views/#{file_path}.html.erb"
+    template 'stylesheet.yml.erb', "static/stylesheets/views/#{file_path}.yml" 
+    template 'javascript.yml.erb', "static/javascripts/views/#{file_path}.yml"
+    file 'view.css.erb', "public/stylesheets/views/#{file_path}.css"
+    file 'view.js.erb', "public/javascripts/views/#{file_path}.js"
   end
+  
   def get_base_path_and_file_name(file_path)
     ["views/#{file_path}".split('/')[0...-1].join('/'), "views/#{file_path}".split('/')[-1]]
   end

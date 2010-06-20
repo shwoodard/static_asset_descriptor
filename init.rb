224a6144e46e17b42a0479a8d5e4b7714859ae37
Sam Woodard <sam.woodard@wildfireapp.com>
2010-06-19 21:49:38 -0700

    update for rails 3 generators, with actually working

--- init.rb
+++
@@ -1,5 +0,0 @@
-require('static_descriptor_parser')
-# Preload Descriptor
-Static::Asset.descriptor
-
-require('helpers')
--- lib/generators/view/view_generator.rb
+++ lib/generators/view/view_generator.rb
@@ -1,21 +1,17 @@
 class ViewGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
+  class_option :haml, :type => :boolean, :default => false
   
   def create_view
-    base_path, file_name = get_base_path_and_file_name(file_path)
-    directory "app/#{base_path}"
-    directory "public/stylesheets/#{base_path}"
-    directory "public/javascripts/#{base_path}"
-    directory "static/stylesheets/#{base_path}"
-    directory "static/javascripts/#{base_path}"
-    template 'view.html.erb', "app/views/#{file_path}.html.erb"
+    if options.haml?
+      template 'view.html.haml', "app/views/#{file_path}.html.haml"
+    else
+      template 'view.html.erb', "app/views/#{file_path}.html.erb"
+    end
+    
     template 'stylesheet.yml.erb', "static/stylesheets/views/#{file_path}.yml" 
     template 'javascript.yml.erb', "static/javascripts/views/#{file_path}.yml"
-    file 'view.css.erb', "public/stylesheets/views/#{file_path}.css"
-    file 'view.js.erb', "public/javascripts/views/#{file_path}.js"
-  end
-  
-  def get_base_path_and_file_name(file_path)
-    ["views/#{file_path}".split('/')[0...-1].join('/'), "views/#{file_path}".split('/')[-1]]
+    copy_file 'view.css.erb', "public/stylesheets/views/#{file_path}.css"
+    copy_file 'view.js.erb', "public/javascripts/views/#{file_path}.js"
   end
 end
